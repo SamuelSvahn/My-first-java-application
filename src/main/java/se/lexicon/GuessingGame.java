530fcb364048b380ea65889ac0988a8b3be99716
SamuelSvahn <samuelsvahn@hotmail.com>
2022-10-11 16:12:16 +0200

    Now my guessing gamin is done

--- src/main/java/se/lexicon/GuessingGame.java
+++ src/main/java/se/lexicon/GuessingGame.java
@@ -2,25 +2,25 @@ package se.lexicon;
 
 import java.util.Scanner;
 
-public class Test {
+public class GuessingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to the guessing game!");
 
         Scanner sc = new Scanner(System.in);
         int k = 5;
-        int i, guess;
+        int i , guess;
 
         int number= (int) (Math.random() * 100);
 
         System.out.println("A number between 1-100 is chosen");
 
-
-        for (i = 0; i < k; i++) {
-            System.out.println("Guess the number");
+        for (i = 1; i <= k; i++) {
+            System.out.println("Guess the number below");
             guess = sc.nextInt();
 
-            if (number == guess ) {
+            if (guess == number ) {
                 System.out.println("You guessed right!");
+                System.out.println("You tried :" + i + " times");
                 break;
 
             } else if (number > guess && i != k - 1) {
