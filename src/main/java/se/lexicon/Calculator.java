cf85267a182dead83b0f691ddcb51d1467d172c6
SamuelSvahn <samuelsvahn@hotmail.com>
2022-10-12 13:28:49 +0200

    Assignment of week 41 "calculator"

---
+++ src/main/java/se/lexicon/Calculator.java
@@ -0,0 +1,66 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean condition = true;
+
+        while (true) {
+
+            CalculatorMathematics.displaymenu();
+            System.out.println("Enter operation type (+ , - , / , *) ");
+            char operationtype = scanner.next().charAt(0);
+
+
+            System.out.println("Enter your number");
+            double number1 = scanner.nextInt();
+            System.out.println("Enter the second number");
+            double number2 = scanner.nextInt();
+
+
+            double result = 0;
+
+            switch (operationtype) {
+                case '+':
+                    result = CalculatorMathematics.addition(number1, number2);
+                    System.out.println("The answer is " + result);
+                    break;
+
+                case '-':
+                    result = CalculatorMathematics.subtraction(number1, number2);
+                    System.out.println("The answer is " + result);
+                    break;
+
+                case '/':
+                    result = CalculatorMathematics.division(number1, number2);
+                    System.out.println("The answer is " + result);
+                    break;
+
+                case '*':
+                    result = CalculatorMathematics.multiplication(number1, number2);
+                    System.out.println("The answer is " + result);
+                    break;
+
+                default:
+                    System.out.println("Something went wrong try again");
+
+            }
+
+
+            System.out.println("Do you want to continue? (Yes or no?");
+            String continueOperation = scanner.next();
+            if (continueOperation.equalsIgnoreCase("no")){
+                System.out.println("Thanks for trying the calculator");
+                condition = false; {
+                    break;
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ src/main/java/se/lexicon/CalculatorMathematics.java
@@ -0,0 +1,24 @@
+package se.lexicon;
+
+public class CalculatorMathematics {
+
+    public static double addition(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public static double subtraction(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public static double division(double number1, double number2) {
+        return number1 / number2;
+    }
+
+    public static double multiplication(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public static void displaymenu() {
+        System.out.println("Welcome to Calculator");
+    }
+}
